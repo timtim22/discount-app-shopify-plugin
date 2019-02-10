a2032fc5c52107061b6f7bf16d505b24dd740cf9
Masterx08 <blackrpr@gmail.com>
2019-02-10 15:26:40 +0500

    mail testing finished

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -5,6 +5,6 @@ class TicketMailer < ApplicationMailer
     @user_name = params[:user_name]
     @user_email  = params[:user_email]
     @user_query = params[:user_query]
-    mail(to: 'arslan_21@rocketmail.com', subject: 'New ticket')
+    mail(to: 'support@marbgroup.com', subject: 'New ticket')
   end
 end
