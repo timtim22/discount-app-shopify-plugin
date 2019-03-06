48a8c2b5f5f348aa64e64e4cbdc60ba8057ec61f
Masterx08 <blackrpr@gmail.com>
2019-03-06 17:18:37 +0500

    removed byebug

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,7 +2,6 @@ class ShopsController < ApplicationController
   include ShopifyApp::WebhookVerification
 
   def uninstall
-    byebug
     Shop.find_by(shopify_domain: params[:myshopify_domain]).destroy
     render json: {success: true}
   end
