6b54a1c04f732a266e25a637c70954dfa8121688
potato_123 <arslan_21@rocketmail.com>
2019-06-22 20:17:49 +0500

    Removed unique option from duplicate worker and set retry to false

--- app/workers/duplicate_store_worker.rb
+++ app/workers/duplicate_store_worker.rb
@@ -1,7 +1,6 @@
 class DuplicateStoreWorker
   include Sidekiq::Worker
-  sidekiq_options retry: 3, lock: :until_executed,
-                  unique_args: ->(args) { [ args.first ] }
+  sidekiq_options retry: false
 
   def perform(*args)
     shop = Shop.find(args[0])
